cf7ac20dd37032844133dc002767d0cf94b5dfa5
mseknibilel <msekni.bilel@gmail.com>
2018-10-26 07:37:07 +0200

    ProgressCheck | Fixed tests

--- projects/client-app/src/app/components/app/app.component.spec.ts
+++ projects/client-app/src/app/components/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from '@client-app/app/components/app/app.component';
+import { SharedModule } from 'shared-lib';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        SharedModule
+      ],
       declarations: [
         AppComponent
       ],
